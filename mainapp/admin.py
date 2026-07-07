0e62248679c74c67f9a558e3a93c65c2bec2c849
bonyborn254 <ezekielbon72@gmail.com>
2026-07-08 02:17:08 +0300

    Register multiple models in Django admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(Gallery)
+admin.site.register(Music)
+admin.site.register(Announcement)
+admin.site.register(Member)
+admin.site.register(ContactMessage)
