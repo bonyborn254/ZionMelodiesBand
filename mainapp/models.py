24a795072934b67c4b622a7b35e142e38c786679
bonyborn254 <ezekielbon72@gmail.com>
2026-07-08 03:23:22 +0300

    Add image field to Event and new Song model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 # Create your models here.
-
 class Event(models.Model):
     title = models.CharField(max_length=200)
     date = models.CharField(max_length=100)
     description = models.TextField()
+    image = models.ImageField(upload_to="events/", blank=True, null=True)
 
     def __str__(self):
         return self.title
-
 
 class Gallery(models.Model):
     title = models.CharField(max_length=100)
@@ -17,17 +16,27 @@ class Gallery(models.Model):
 
     def __str__(self):
         return self.title
+        
+class Song(models.Model):
+    WORSHIP = "Worship"
+    PRAISE = "Praise"
 
+    TYPES = [
+        (WORSHIP, "Worship"),
+        (PRAISE, "Praise"),
+    ]
 
-class Music(models.Model):
     title = models.CharField(max_length=200)
-    key = models.CharField(max_length=100)
     leader = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
+    music_key = models.CharField(max_length=50)
+    category = models.CharField(
+        max_length=20,
+        choices=TYPES,
+        default=WORSHIP
+    )
 
     def __str__(self):
         return self.title
-
 
 class Announcement(models.Model):
     title = models.CharField(max_length=200)
@@ -55,7 +64,7 @@ class ContactMessage(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=30)
     message = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+    submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.email}"
